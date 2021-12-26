3c39787f1fd9062a877d4d8036428c3cb5a19812
Ryoma Hattori <rhattori0204@gmail.com>
2021-12-25 20:13:11 -0800

    bug fix

--- PatchWarp/utils/Akinori_Mitani_motion_correct_files/get_frame_tag_from_info.m
+++ PatchWarp/utils/Akinori_Mitani_motion_correct_files/get_frame_tag_from_info.m
@@ -2,11 +2,12 @@ function frame_tag = get_frame_tag_from_info(info)
     frame_tag = NaN(size(info));
     for i=1:numel(info)
         if(isfield(info,'ImageDescription'))
-            tmp = str2double(regexp( ...
-                    info(i).ImageDescription,...
-                    'Frame Tag = (\d*)','tokens','once'));
-            if(~isempty(tmp))
-                frame_tag(i) = tmp;
+            try
+                tmp = str2double(regexp(info(i).ImageDescription,'Frame Tag = (\d*)','tokens','once'));
+                if(~isempty(tmp))
+                    frame_tag(i) = tmp;
+                end
+            catch
             end
         end
     end
