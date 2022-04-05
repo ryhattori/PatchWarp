36afcda2c9c4af82c940336e4e39f32c80047a4d
Ryoma Hattori <rhattori0204@gmail.com>
2022-04-04 18:00:42 -0700

    Organized input params into a structure var

--- PatchWarp/patchwarp.m
+++ PatchWarp/patchwarp.m
@@ -1,4 +1,4 @@
-function patchwarp(source_path, save_path, n_ch, align_ch, save_ch, run_rigid_mc, run_affine_wc, rigid_norm_method, rigid_norm_radius, rigid_template_block_num, rigid_template_threshold, rigid_template_tiffstack_num, rigid_template_center_frac, affine_norm_radius, warp_template_tiffstack_num, warp_movave_tiffstack_num, warp_blocksize, warp_overlap_pix_frac, n_split4warpinit, edge_remove_pix, affinematrix_abssum_threshold, affinematrix_abssum_jump_threshold, affinematrix_rho_threshold, affinematrix_medfilt_tiffstack_num, transform, warp_pyramid_levels, warp_pyramid_iterations, downsample_frame_num, worker_num, network_temp_copy)
+function patchwarp(ops)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % PatchWarp
 % -------------------
@@ -7,25 +7,142 @@ function patchwarp(source_path, save_path, n_ch, align_ch, save_ch, run_rigid_mc
 % Email: rhattori0204@gmail.com
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-delete(gcp('nocreate'));
-parpool(worker_num);
+%% Check if all input parameters exist
+if ~isfield(ops, 'source_path')
+    error('Specify ops.source_path!')
+end
+if ~isfield(ops, 'save_path')
+    error('Specify ops.save_path!')
+end
+if ~isfield(ops, 'n_ch')
+    disp('ops.n_ch is not specified. Running with ops.n_ch = 1')
+    ops.n_ch = 1;
+end
+if ~isfield(ops, 'align_ch')
+    disp('ops.align_ch is not specified. Running with ops.align_ch = 1')
+    ops.align_ch = 1;
+end
+if ~isfield(ops, 'save_ch')
+    disp('ops.save_ch is not specified. Running with ops.save_ch = 1')
+    ops.save_ch = 1;
+end
+if ~isfield(ops, 'run_rigid_mc')
+    disp('ops.run_rigid_mc is not specified. Running with ops.run_rigid_mc = 1')
+    ops.run_rigid_mc = 1;
+end
+if ~isfield(ops, 'run_affine_wc')
+    disp('ops.run_affine_wc is not specified. Running with ops.run_affine_wc = 1')
+    ops.run_affine_wc = 1;
+end
+if ~isfield(ops, 'rigid_norm_method')
+    disp('ops.rigid_norm_method is not specified. Running with ops.rigid_norm_method = rank')
+    ops.rigid_norm_method = 'rank';
+end
+if ~isfield(ops, 'rigid_norm_radius')
+    ops.rigid_norm_radius = '32';
+end
+if ~isfield(ops, 'rigid_template_block_num')
+    disp('ops.rigid_template_block_num is not specified. Running with ops.rigid_template_block_num = 3')
+    ops.rigid_template_block_num = 3;
+end
+if ~isfield(ops, 'rigid_template_threshold')
+    disp('ops.rigid_template_threshold is not specified. Running with ops.rigid_template_threshold = 0.2')
+    ops.rigid_template_threshold = 0.2;
+end
+if ~isfield(ops, 'rigid_template_tiffstack_num')
+    disp('ops.rigid_template_tiffstack_num is not specified. Running with ops.rigid_template_tiffstack_num = 3')
+    ops.rigid_template_tiffstack_num = 3;
+end
+if ~isfield(ops, 'rigid_template_center_frac')
+    disp('ops.rigid_template_center_frac is not specified. Running with ops.rigid_template_center_frac = 0.8')
+    ops.rigid_template_center_frac = 0.8;
+end
+if ~isfield(ops, 'affine_norm_radius')
+    disp('ops.affine_norm_radius is not specified. Running with ops.affine_norm_radius = 32')
+    ops.affine_norm_radius = 32;
+end
+if ~isfield(ops, 'warp_template_tiffstack_num')
+    disp('ops.warp_template_tiffstack_num is not specified. Running with ops.warp_template_tiffstack_num = 5')
+    ops.warp_template_tiffstack_num = 5;
+end
+if ~isfield(ops, 'warp_movave_tiffstack_num')
+    disp('ops.warp_movave_tiffstack_num is not specified. Running with ops.warp_movave_tiffstack_num = 1')
+    ops.warp_movave_tiffstack_num = 1;
+end
+if ~isfield(ops, 'warp_blocksize')
+    disp('ops.warp_blocksize is not specified. Running with ops.warp_blocksize = 8')
+    ops.warp_blocksize = 8;
+end
+if ~isfield(ops, 'warp_overlap_pix_frac')
+    disp('ops.warp_overlap_pix_frac is not specified. Running with ops.warp_overlap_pix_frac = 0.15')
+    ops.warp_overlap_pix_frac = 0.15;
+end
+if ~isfield(ops, 'n_split4warpinit')
+    disp('ops.n_split4warpinit is not specified. Running with ops.n_split4warpinit = 6')
+    ops.n_split4warpinit = 6;
+end
+if ~isfield(ops, 'edge_remove_pix')
+    ops.edge_remove_pix = 0;
+end
+if ~isfield(ops, 'affinematrix_abssum_threshold')
+    ops.affinematrix_abssum_threshold = 50;
+end
+if ~isfield(ops, 'affinematrix_abssum_jump_threshold')
+    ops.affinematrix_abssum_jump_threshold = 10;
+end
+if ~isfield(ops, 'affinematrix_rho_threshold')
+    ops.affinematrix_rho_threshold = 0.5;
+end
+if ~isfield(ops, 'affinematrix_medfilt_tiffstack_num')
+    disp('ops.affinematrix_medfilt_tiffstack_num is not specified. Running with ops.affinematrix_medfilt_tiffstack_num = 1 (no filtering). Use larger value if warp result is unstable.')
+    ops.affinematrix_medfilt_tiffstack_num = 1;
+end
+if ~isfield(ops, 'transform')
+    disp('ops.transform is not specified. Running with ops.transform = affine')
+    ops.transform = 'affine';
+end
+if ~isfield(ops, 'warp_pyramid_levels')
+    ops.warp_pyramid_levels = 1;
+end
+if ~isfield(ops, 'warp_pyramid_iterations')
+    disp('ops.warp_pyramid_iterations is not specified. Running with ops.warp_pyramid_iterations = 50')
+    ops.warp_pyramid_iterations = 50;
+end
+if ~isfield(ops, 'downsample_frame_num')
+    disp('ops.downsample_frame_num is not specified. Running with ops.downsample_frame_num = 50')
+    ops.downsample_frame_num = 50;
+end
+if ~isfield(ops, 'worker_num')
+    disp('ops.worker_num is not specified. Running with maximum worker number. Limit the number if you see out of memory error.')
+    ops.worker_num = parcluster('local').NumWorkers;
+end
+if ~isfield(ops, 'network_temp_copy')
+    ops.network_temp_copy = 1;
+end
 
-save_path_prewarp = fullfile(save_path, 'pre_warp');
-save_path_postwarp = fullfile(save_path, 'post_warp');
-if run_rigid_mc == 1
+%% Start parallel pool
+delete(gcp('nocreate'));
+parpool(ops.worker_num);
+
+%% Run rigid correction
+save_path_prewarp = fullfile(ops.save_path, 'pre_warp');
+save_path_postwarp = fullfile(ops.save_path, 'post_warp');
+if ops.run_rigid_mc == 1
     disp('Performing rigid motion correction...')
-    patchwarp_rigid(source_path, save_path_prewarp, n_ch, align_ch, save_ch, rigid_norm_method, rigid_norm_radius, rigid_template_block_num, rigid_template_threshold, rigid_template_tiffstack_num, rigid_template_center_frac, downsample_frame_num, network_temp_copy, []);
+    patchwarp_rigid(ops.source_path, save_path_prewarp, ops.n_ch, ops.align_ch, ops.save_ch, ops.rigid_norm_method, ops.rigid_norm_radius, ops.rigid_template_block_num, ops.rigid_template_threshold, ops.rigid_template_tiffstack_num, ops.rigid_template_center_frac, ops.downsample_frame_num, ops.network_temp_copy, []);
 else
     disp('Skipping rigid motion correction...')
 end
 
-if run_affine_wc == 1
+%% Run warp correction
+if ops.run_affine_wc == 1
     disp('Performing warp correction...')
-    patchwarp_affine(save_path_prewarp, save_path_postwarp, n_ch, align_ch, affine_norm_radius, warp_template_tiffstack_num, warp_movave_tiffstack_num, warp_blocksize, warp_overlap_pix_frac, n_split4warpinit, edge_remove_pix, affinematrix_abssum_threshold, affinematrix_abssum_jump_threshold, affinematrix_rho_threshold, affinematrix_medfilt_tiffstack_num, transform, warp_pyramid_levels, warp_pyramid_iterations, downsample_frame_num, worker_num, network_temp_copy);
+    patchwarp_affine(save_path_prewarp, save_path_postwarp, ops.n_ch, ops.align_ch, ops.affine_norm_radius, ops.warp_template_tiffstack_num, ops.warp_movave_tiffstack_num, ops.warp_blocksize, ops.warp_overlap_pix_frac, ops.n_split4warpinit, ops.edge_remove_pix, ops.affinematrix_abssum_threshold, ops.affinematrix_abssum_jump_threshold, ops.affinematrix_rho_threshold, ops.affinematrix_medfilt_tiffstack_num, ops.transform, ops.warp_pyramid_levels, ops.warp_pyramid_iterations, ops.downsample_frame_num, ops.worker_num, ops.network_temp_copy);
 else
     disp('Skipping warp correction...')
 end
 
+%% Shutdown parallel pool
 delete(gcp('nocreate'))
         
     
--- PatchWarp/patchwarp_demo.m
+++ PatchWarp/patchwarp_demo.m
@@ -12,42 +12,35 @@ addpath(genpath(patchwarp_path))
 
 %% Specifiy source directory and saving directory
 % Image data needs to be tiff stack files.
-source_path = 'Z:\People\Ryoma\MC_data\171008\RH825';    % Directory that contains original tif stack files
-save_path = 'Z:\People\Ryoma\MC_data\171008\RH825\corrected';    % Directory where motion corrected images will be saved
+ops.source_path = 'Z:\People\Ryoma\MC_data\171008\RH825';    % Directory that contains original tiff stack files
+ops.save_path = 'Z:\People\Ryoma\MC_data\171008\RH825\corrected';    % Directory where motion corrected images will be saved
 
 %% Set general parameters
 % n_ch:                     Number of saved PMT channels. Set this to 2 for if the session was 2-color imaging.
 % align_ch:                 Imaging channel to be aligned
 % save_ch:                  Imaging channel to be saved
-% run_rigid_mc:             skip rigid motion correction (0), or do ridig motion correction (1)
-% run_affine_wc:            skip warp correction (0), or do warp correction(1)
-n_ch = 1;
-align_ch = 1;
-save_ch = 1;
-run_rigid_mc = 1;
-run_affine_wc = 1;
+% run_rigid_mc:             Skip rigid motion correction (0), or do rigid motion correction (1). Skip only if rigid correction was done previously.
+% run_affine_wc:            Skip warp correction (0), or do warp correction(1). Note that rigid correction needs to be done to run this warp correction.
+ops.n_ch = 1;
+ops.align_ch = 1;
+ops.save_ch = 1;
+ops.run_rigid_mc = 1;
+ops.run_affine_wc = 1;
 
 %% Set parameter for downsampled motion corrected tiff stacks
 % Downsampled movie will be created after rigid motion correction and affine correction.
 % These tiff stack files can be used for visual inspection of the motion correction quality.
 % downsample_frame_num:     Window size for non-overlapping moving averaging.
-downsample_frame_num = 50; 
-
-%% Option to disable making local copies
-% On Linux or Mac OS, tiff files will be temporarily copied in a temporary folder by default if they are 
-% in a network drive because the pipeline is faster this way in most cases. This behavior can be disabled 
-% by this setting network_temp_copy = 0. More efficient tiff reader (ScanImageTiffreader) will be used on Windows, and 
-% it directly reads tiff in a network drive without making local copies ('network_temp_copy' parameter will be ignored on Windows).
-network_temp_copy = 1;
+ops.downsample_frame_num = 50; 
 
 %% Specify the number of workers for parallel processing
 % worker_num:   Specify the number of workers that processing registrations in parallel. Assign all available CPU cores for the maximum processing
 %               speed (e.g. parcluster('local').NumWorkers). If you get 'Out of Memory' error due to limited RAM, assign smaller number of workers.
-worker_num = parcluster('local').NumWorkers; 
+ops.worker_num = parcluster('local').NumWorkers; 
 
 %% Set parameter for rigid motion correction
 % rigid_norm_method:            'rank' or 'local'. When PMT noise is high,'rank' method probably works better. Try 'local' when 'rank' does not work well.
-% rigid_norm_radius:            Radius of a circular filter for 'local' normalization method. This used for local intensity normalization before 
+% rigid_norm_radius:            Radius of a circular filter for 'local' normalization method. This is used for local intensity normalization before 
 %                               hill climbing algorithm. This parameter will be ignored when the method is 'rank'.
 % rigid_template_tiffstack_num: Number of tif stack files used for estimating template images. For example, if each tif stack file has 500
 %                               frames, each template image will be created using 500*[rigid_template_tiffstack_num] frames.
@@ -59,12 +52,12 @@ worker_num = parcluster('local').NumWorkers;
 %                               be ignored when obtaining the final template image.
 % rigid_template_center_frac:   Central fraction of a template image used for registration. For example, 10% of pixels are ignored from each edge
 %                               when rigid_template_center_frac = 0.8.
-rigid_norm_method = 'rank';
-rigid_norm_radius = 32;
-rigid_template_tiffstack_num = 5;   % Note that this number needs to be smaller than the total number of tif stack files in the directory. 
-rigid_template_block_num = 5;   % This must be an odd number (1, 3, 5,...). This parameter will be ignored if [number of tif stack files] < 3.
-rigid_template_threshold = 0.2;
-rigid_template_center_frac = 0.8;
+ops.rigid_norm_method = 'rank';
+ops.rigid_norm_radius = 32;
+ops.rigid_template_tiffstack_num = 5;   % Note that this number needs to be smaller than the total number of tif stack files in the directory. 
+ops.rigid_template_block_num = 5;   % This must be an odd number (1, 3, 5,...). This parameter will be ignored if [number of tif stack files] < 3.
+ops.rigid_template_threshold = 0.2;
+ops.rigid_template_center_frac = 0.8;
 
 %% Set parameter for warp correction
 % Same transformation will be applied to all frames in each tiff stack by default because of the slow nature of distortion.
@@ -88,38 +81,40 @@ rigid_template_center_frac = 0.8;
 %                                       edges that were not scanned by a microscope. 
 % n_split4warpinit:                     Sessions are split to this number, and initial affine transformation matrices are re-estimated for each block.
 %                                       Gradient descent algorithm find the best matrices from the initial guess.
-% affinematrix_abssum_threshold:        If the sum of absolute values of all elements in an estimated affine transformation matrix exceeds 
-%                                       affinematrix_abssum_threshold, the matrix will be ignored before median temporal filtering.
-% affinematrix_abssum_jump_threshold:   Ignore estimated affine matrices when the estimated matrix is very different from the matrix from the adjacent frame. 
-%                                       If the sum(abs(matrix1 - matrix2)) > affinematrix_abssum_jump_threshold, the matrix will be ignored.
 % affinematrix_rho_threshold:           If the enhanced correlation coefficient between the template and transformed subfield is less than or equal to
 %                                       this threshold, the matrix will be ignored before median temporal filtering.
 % affinematrix_medfilt_tiffstack_num:   Window size (number of tif stack files) that are used for median temporal filtering of affine transformation matrices.
 %                                       1 (no median filtering) should work in most cases. Increase this number if warp corrections fail in some blocks.   
-transform = 'affine';
-affine_norm_radius = 32;
-warp_pyramid_levels = 1;
-warp_pyramid_iterations = 50;
-warp_template_tiffstack_num = 7;    % Do not exceed the number of tiff stack files in the source directory. 
+% affinematrix_abssum_threshold:        If the sum of absolute values of all elements in an estimated affine transformation matrix exceeds 
+%                                       affinematrix_abssum_threshold, the matrix will be ignored before median temporal filtering.
+% affinematrix_abssum_jump_threshold:   Ignore estimated affine matrices when the estimated matrix is very different from the matrix from the adjacent frame. 
+%                                       If the sum(abs(matrix1 - matrix2)) > affinematrix_abssum_jump_threshold, the matrix will be ignored.
+ops.transform = 'affine';
+ops.affine_norm_radius = 32;
+ops.warp_pyramid_levels = 1;
+ops.warp_pyramid_iterations = 50;
+ops.warp_template_tiffstack_num = 7;    % Do not exceed the number of tiff stack files in the source directory. 
                                     % If there is only 1 tiff stack in the source directroy, the # of frames set by 'downsample_frame_num' will be treated 
                                     % as a single block ([downsample_frame_num]*[warp_template_tiffstack_num] frames will be used to make the template).   
-warp_movave_tiffstack_num = 1;
-warp_blocksize = 8;     % For moderate distortion, use small number (e.g. 2-4). For severe distortion, use large number. Note that the processing time takes
+ops.warp_movave_tiffstack_num = 1;
+ops.warp_blocksize = 8;     % For moderate distortion, use small number (e.g. 2-4). For severe distortion, use large number. Note that the processing time takes
                         % much longer if you use a large blocksize.
-warp_overlap_pix_frac = 0.15;
-edge_remove_pix = 0;
-n_split4warpinit = 6;   % This must be an even number.
-affinematrix_abssum_threshold = 50;
-affinematrix_abssum_jump_threshold = 10;
-affinematrix_rho_threshold = 0.5;
-affinematrix_medfilt_tiffstack_num = 1;   
+ops.warp_overlap_pix_frac = 0.15;
+ops.edge_remove_pix = 0;
+ops.n_split4warpinit = 6;   % This must be an even number.
+ops.affinematrix_rho_threshold = 0.5;
+ops.affinematrix_medfilt_tiffstack_num = 1;   % Increase this parameter if warp correction fails in some subfields.
+ops.affinematrix_abssum_threshold = 50;
+ops.affinematrix_abssum_jump_threshold = 10;
 
+%% Option to disable making local copies
+% On Linux or Mac OS, tiff files will be temporarily copied in a temporary folder by default if they are 
+% in a network drive because the pipeline is faster this way in many cases. This behavior can be disabled 
+% by this setting network_temp_copy = 0. More efficient tiff reader (ScanImageTiffreader) will be used on Windows, and 
+% it directly reads tiff in a network drive without making local copies ('network_temp_copy' parameter will be ignored on Windows).
+ops.network_temp_copy = 1;
 
 %% Run PatchWarp
-patchwarp(source_path, save_path, n_ch, align_ch, save_ch, run_rigid_mc, run_affine_wc,...
-    rigid_norm_method, rigid_norm_radius, rigid_template_block_num, rigid_template_threshold, rigid_template_tiffstack_num, rigid_template_center_frac,...
-    affine_norm_radius, warp_template_tiffstack_num, warp_movave_tiffstack_num, warp_blocksize, warp_overlap_pix_frac, n_split4warpinit, edge_remove_pix,...
-    affinematrix_abssum_threshold, affinematrix_abssum_jump_threshold, affinematrix_rho_threshold, affinematrix_medfilt_tiffstack_num, transform, warp_pyramid_levels, warp_pyramid_iterations,...
-    downsample_frame_num, worker_num, network_temp_copy);
+patchwarp(ops);
 
 
